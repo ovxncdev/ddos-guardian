7d84b8cbdbb1eb6bf8a9ae26c8a1c340b8344eaa
ovxncdev <adeniyireal1icloud@gmail.com>
2026-01-08 14:20:26 -0800

    Create index..js

---
+++ src/utils/index..js
@@ -0,0 +1,14 @@
+/**
+ * Utilities Index
+ * 
+ * Usage:
+ *   const { environment, paths } = require('./utils');
+ */
+
+const environment = require('./environment');
+const paths = require('./paths');
+
+module.exports = {
+    environment,
+    paths,
+};
