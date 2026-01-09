579af863b1a3730f46c11924a13e2d9bf434dd01
ovxncdev <adeniyireal1icloud@gmail.com>
2026-01-08 18:00:25 -0800

    Update index.js

--- src/core/index.js
+++ src/core/index.js
@@ -2,15 +2,17 @@
  * Core Module Index
  * 
  * Usage:
- *   const { RateLimiter, IpTracker, BotDetector } = require('./core');
+ *   const { RateLimiter, IpTracker, BotDetector, Proxy } = require('./core');
  */
 
 const IpTracker = require('./ip-tracker');
 const RateLimiter = require('./rate-limiter');
 const BotDetector = require('./bot-detector');
+const Proxy = require('./proxy');
 
 module.exports = {
     IpTracker,
     RateLimiter,
     BotDetector,
+    Proxy,
 };
